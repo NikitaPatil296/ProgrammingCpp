9a55236a2cc5e8b44c65a164421a8b43828e93d6
Nikita Patil <patilnikita296@gmail.com>
2025-09-18 19:32:43 +0200

    Added char

---
+++ Char/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    char ch;
+    std::cout << "Enter any character:" << std::endl;
+    std::cin >> ch;
+    std::cout << "Your character: " << ch << std::endl;
+    return 0;
+}
